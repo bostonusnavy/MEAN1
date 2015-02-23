ea5b15bc62435e715eb0e8e3a49569bdfdc7886a
bostonusnavy <robert.b.degroot@gmail.com>
2015-02-23 09:03:52 -0500

    UI buffed, Angular data added, filtering

--- public/app/app.js
+++ public/app/app.js
@@ -8,14 +8,7 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
     $routeProvider
         // "templateURL:" places file specified into "(ng-view)"
         // "controller:" specifies which controller to use
-        .when('/', { templateUrl: '/partials/main', controller: 'pizza'});
+        .when('/', { templateUrl: '/partials/main', controller: 'meanMainCtrl'});
         // you can '.' chain several "whens" together to handle different front-side routes before ';'
 });
 
-angular.module('app').controller('mainCtrl', function($scope) {
-    $scope.myVar = "Hello Angular!";
-});
-
-angular.module('app').controller('pizza', function($scope) {
-    $scope.myVar = "Hello pizza!";
-});
