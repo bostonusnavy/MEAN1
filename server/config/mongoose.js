2e0926557273a8c06d424efb5be36223b1f56063
bostonusnavy <robert.b.degroot@gmail.com>
2015-03-05 01:49:42 -0500

    User creation refactored, pushed to server-side validation. All cases are pushed to lower case, creation and login. Everthing tested and working

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,46 +1,21 @@
 var mongoose = require('mongoose'),
-    encrypt = require('../utilities/encryption');
+    userModel = require('../models/User');
 
 module.exports = function (config) {
     mongoose.connect(config.db);
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'Connection error...'));
-    db.once('open', function callback () {
+    db.once('open', function callback() {
         console.log('MEAN1 DB Opened!');
     });
 
-    var userSchema = mongoose.Schema({
-        firstName: String,
-        lastName: String,
-        username: String,
-        salt: String,
-        hashed_pwd: String,
-        roles: [String]
+    userModel.createDefaultUsers();
 
-    });
-    userSchema.methods = {
-        authenticate: function(passwordToMatch) {
-            return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-        }
-    };
-
-    var User = mongoose.model('User', userSchema);
-
-    User.find({}).exec(function (err, collection) {
-        if(collection.length === 0) {
-            var salt, hash;
-            salt = encrypt.createSalt();
-            hash = encrypt.hashPwd(salt, 'rdegroot');
-            User.create({firstName:'Rob', lastName:'DeGroot', username:'rdegroot', salt: salt, hashed_pwd: hash, roles: ['admin']});
-            salt = encrypt.createSalt();
-            hash = encrypt.hashPwd(salt, 'jsmith');
-            User.create({firstName:'John', lastName:'Smith', username:'jsmith', salt: salt, hashed_pwd: hash, roles: []});
-            salt = encrypt.createSalt();
-            hash = encrypt.hashPwd(salt, 'cklein');
-            User.create({firstName:'Calvin', lastName:'Klein', username:'cklein', salt: salt, hashed_pwd: hash});
-        }
-    })
 };
+
+
+
+
 
 //var messageSchema = mongoose.Schema({message: String});
 //var Message = mongoose.model('Message', messageSchema);
